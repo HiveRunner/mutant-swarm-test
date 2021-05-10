5b4cb971f691b2c9f2dfb06e4cab4c0a6d1e0b14
shermosa <shermosa@hotels.com>
2021-05-10 11:27:29 +0100

    update versions and reproduce error

--- mutant-swarm-test-junit5/src/test/java/com/expediagroup/dataplatform/mutantswarm/test/junit5/Junit5Test.java
+++ mutant-swarm-test-junit5/src/test/java/com/expediagroup/dataplatform/mutantswarm/test/junit5/Junit5Test.java
@@ -19,6 +19,7 @@ package com.expediagroup.dataplatform.mutantswarm.test.junit5;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
@@ -40,18 +41,18 @@ public class Junit5Test {
 
   @HiveSQL(files = { "mutantSwarmTest/scriptToTest1.sql", "mutantSwarmTest/scriptToTest2.sql" })
   public HiveShell hiveShell;
-  
-  @MutantSwarmTest
-  public void tests() {
-    List<String> result = hiveShell.executeQuery("SELECT c FROM foobar");
-    List<String> expected = Arrays.asList("true", "false");
-    assertEquals(expected, result);
-  }
 
   @MutantSwarmTest
   public void test() {
     List<String> result = hiveShell.executeQuery("SELECT * FROM bar");
     List<String> expected = Arrays.asList("1\ttrue", "3\ttrue", "3\tfalse", "5\tfalse");
+    assertEquals(expected, result);
+  }
+  
+  @MutantSwarmTest
+  public void test2() {
+    List<String> result = hiveShell.executeQuery("SELECT c FROM foobar");
+    List<String> expected = Arrays.asList("true", "false");
     assertEquals(expected, result);
   }
 
