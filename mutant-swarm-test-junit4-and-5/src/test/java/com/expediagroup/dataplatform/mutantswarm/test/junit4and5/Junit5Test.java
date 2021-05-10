eb55a249872010c291e2bf050fbf1f696196fd0e
shermosa <shermosa@hotels.com>
2021-05-10 14:08:07 +0100

    readme and tidy up spaces

--- mutant-swarm-test-junit4-and-5/src/test/java/com/expediagroup/dataplatform/mutantswarm/test/junit4and5/Junit5Test.java
+++ mutant-swarm-test-junit4-and-5/src/test/java/com/expediagroup/dataplatform/mutantswarm/test/junit4and5/Junit5Test.java
@@ -41,14 +41,14 @@ public class Junit5Test {
 
   @HiveSQL(files = { "mutantSwarmTest/scriptToTest1.sql", "mutantSwarmTest/scriptToTest2.sql" })
   public HiveShell hiveShell;
- 
+
   @MutantSwarmTest
   public void test() {
     List<String> result = hiveShell.executeQuery("SELECT * FROM bar");
     List<String> expected = Arrays.asList("1\ttrue", "3\ttrue", "3\tfalse", "5\tfalse");
     assertEquals(expected, result);
   }
-  
+
   @MutantSwarmTest
   public void test2() {
     List<String> result = hiveShell.executeQuery("SELECT c FROM foobar");
